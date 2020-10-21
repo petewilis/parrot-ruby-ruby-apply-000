bddc925ff96e27035a14805d9225426ad4f23353
Jonathan Mattos <jonmattos2014@gmail.com>
2020-10-21 02:48:21 +0000

    Automatically backed up by Learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,12 +1,7 @@
 require_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
 
-describe '#parrot' do
-  it 'should output "Squawk!" to the terminal when called without any arguments' do
-    expect($stdout).to receive(:puts).with("Squawk!")
-    parrot
-  end
-
+ 
   it 'should return the default phrase, "Squawk!" when called without any arguments' do
     phrase = parrot
     expect(phrase).to eq("Squawk!")
